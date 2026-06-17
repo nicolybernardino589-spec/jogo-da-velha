60f9297a1acf3706062b80dfde0ec80a01421041
nicolybernardino589-spec <nicolybernardino589@gmail.com>
2026-06-17 20:18:42 -0300

    Update jogo-da-velha.c

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void iniciarJogo(int argc, char *argv[]);
-
 int main(int argc, char *argv[])
 {
-    iniciarJogo(argc, argv);
 
     printf("\nCriado por Nicoly Bernardino\n");
     return 0;
